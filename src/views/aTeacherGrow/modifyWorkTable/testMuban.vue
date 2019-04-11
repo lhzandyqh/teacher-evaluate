f1b5b2357260f1605bfe36d920a799246e4731ab
lannsiter <lannisteryqh@163.com>
2019-04-11 14:20:55 +0800

    increase the workTask function by use elementUI table

---
+++ src/views/aTeacherGrow/modifyWorkTable/testMuban.vue
@@ -0,0 +1,53 @@
+<template>
+  <el-dialog :visible.sync="dialogFormVisible" title="收货地址">
+    <el-form :model="form">
+      <el-form-item :label-width="formLabelWidth" label="活动名称">
+        <el-input v-model="form.name" autocomplete="off"/>
+      </el-form-item>
+      <el-form-item :label-width="formLabelWidth" label="活动区域">
+        <el-select v-model="form.region" placeholder="请选择活动区域">
+          <el-option label="区域一" value="shanghai"/>
+          <el-option label="区域二" value="beijing"/>
+        </el-select>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="dialogFormVisible = false">取 消</el-button>
+      <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialogFormVisible: false,
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      },
+      formLabelWidth: '120px'
+    }
+  },
+  methods: {
+    handleClose(done) {
+      this.$confirm('确认关闭？')
+        .then(_ => {
+          done()
+        })
+        .catch(_ => {})
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
