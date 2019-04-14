69561f128c5dcf61dd27f6750795c04158f88c02
吴恩廷 <377954252@qq.com>
2019-04-14 11:24:26 +0800

    合了下代码

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,7 @@ import Layout from '@/views/layout/Layout'
 /* Router Modules */
 import componentsRouter from './modules/components'
 import chartsRouter from './modules/charts'
-import tableRouter from './modules/table'
+// import tableRouter from './modules/table'
 import nestedRouter from './modules/nested'
 
 /** note: Submenu only appear when children.length>=1
@@ -283,7 +283,7 @@ export const asyncRouterMap = [
   {
     path: '/icon',
     component: Layout,
-    // hidden: true,
+    hidden: true,
     children: [
       {
         path: 'index',
@@ -298,7 +298,7 @@ export const asyncRouterMap = [
   componentsRouter,
   chartsRouter,
   nestedRouter,
-  tableRouter,
+  // tableRouter,
 
   {
     path: '/example',
